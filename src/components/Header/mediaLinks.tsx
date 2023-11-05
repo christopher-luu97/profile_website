2f42401c1ed39387d3f64aa0cc8e67db7ce1cbd3
christopher-luu97 <christopher.luu1337@gmail.com>
2023-11-05 15:40:06 +1100

    Add links

--- src/components/Header/mediaLinks.tsx
+++ src/components/Header/mediaLinks.tsx
@@ -3,24 +3,24 @@ export const mediaLinks = [
     name: "LinkedIn",
     description: "Link to my linkedin",
     icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg",
-    link: "/",
+    link: "www.linkedin.com/in/christopher-luu-542691149",
   },
   {
     name: "GitHub",
     description: "Link to my GitHub",
     icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
-    link: "/",
+    link: "https://github.com/christopher-luu97",
   },
   {
     name: "Medium",
     description: "Link to my Medium",
     icon: "https://www.svgrepo.com/show/512457/medium-149.svg",
-    link: "/",
+    link: "https://medium.com/@cluu.codes1337",
   },
   {
     name: "Youtube",
     description: "Link to my YouTube",
     icon: "https://www.svgrepo.com/show/13671/youtube.svg",
-    link: "/",
+    link: "https://www.youtube.com/@chris_codes",
   },
 ];
--- src/components/LandingPage/ProjectData.ts
+++ src/components/LandingPage/ProjectData.ts
@@ -12,21 +12,36 @@ export interface Project {
 
 export const projects: Project[] = [
   {
-    id: "project-1",
-    name: "Project One",
-    projectDescription: "This is a short description of the first project.",
-    imageUrl: "path_to_your_project_image.jpg", // replace with your image path
-    articleUrl: "/",
-    githubUrl: "/",
-    youtubeUrl: "/",
+    id: "project-003",
+    name: "Code search and question answering",
+    projectDescription:
+      "Vector search on code repositories with Marqo and question answering with a large language model, all offline",
+    imageUrl: "https://i.ytimg.com/vi/tcz7p1Tx0LY/maxresdefault.jpg", // replace with your image path
+    articleUrl:
+      "https://medium.com/@cluu.codes1337/code-search-and-question-answering-using-marqo-and-a-locally-hosted-llm-00d0dda8e8e7",
+    githubUrl: "https://github.com/christopher-luu97/repo_search",
+    youtubeUrl:
+      "https://www.youtube.com/watch?v=tcz7p1Tx0LY&ab_channel=chriscodes",
   },
   {
-    id: "project-2",
-    name: "Project Two",
-    projectDescription: "This is a short description of the second project.",
-    imageUrl: "path_to_your_project_image.jpg", // replace with your image path
-    articleUrl: "/",
-    githubUrl: "/",
-    youtubeUrl: "/",
+    id: "project-002",
+    name: "Search your transcripts",
+    projectDescription:
+      "Efficiently search through transcriptions with Weaviate",
+    imageUrl: "https://i.ytimg.com/vi/1Rlz3VlKcsk/maxresdefault.jpg", // replace with your image path
+    articleUrl:
+      "https://medium.com/@cluu.codes1337/how-i-made-a-complete-vector-search-web-application-with-weaviate-d88e3b667bb",
+    githubUrl: "https://github.com/christopher-luu97/TranscriptionSearch",
+    youtubeUrl:
+      "https://www.youtube.com/watch?v=1Rlz3VlKcsk&ab_channel=chriscodes",
+  },
+  {
+    id: "project-001",
+    name: "Transcription web application",
+    projectDescription: "Transcribe youtube videos all in one place",
+    imageUrl: "https://i.ytimg.com/vi/FIFocNaa4mo/maxresdefault.jpg", // replace with your image path
+    githubUrl: "https://github.com/christopher-luu97/transcription_webapp",
+    youtubeUrl:
+      "https://www.youtube.com/watch?v=FIFocNaa4mo&ab_channel=chriscodes",
   },
 ];
