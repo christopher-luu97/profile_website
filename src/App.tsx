e35212929917b7820fcd159ee72822be1c747582
christopher-luu97 <christopher.luu1337@gmail.com>
2023-11-05 17:40:49 +1100

    Comment out pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Header } from "./components/Header/Header";
 import { Footer } from "./components/Footer/Footer";
 import { LandingPage } from "./components/LandingPage/LandingPage";
-import Projects from "./components/Pages/Projects";
-import Blog from "./components/Pages/Blog";
+// import Projects from "./components/Pages/Projects";
+// import Blog from "./components/Pages/Blog";
 
 function App() {
   return (
-    <Router>
+    <>
       <Header />
       <Routes>
-        <Route path="/christopher-luu97.github.io" element={<LandingPage />} />
-        <Route
+        <Route path="/" element={<LandingPage />} />
+        {/* <Route
           path="/christopher-luu97.github.io/projects"
           element={<Projects />}
         />
-        <Route path="/christopher-luu97.github.io/blog" element={<Blog />} />
+        <Route path="/christopher-luu97.github.io/blog" element={<Blog />} /> */}
       </Routes>
       <Footer />
-    </Router>
+    </>
   );
 }
 
